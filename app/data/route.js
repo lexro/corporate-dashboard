ab0a3672e1ce7b428d9b223523e39dd966bc431e
Lex Lacson <lex.lacson.dev@gmail.com>
2017-01-02 09:58:01 -1000

    hook up mock-data

--- app/data/route.js
+++ app/data/route.js
@@ -1,31 +1,58 @@
 import Ember from 'ember';
 import Table from 'ember-light-table';
+import request from 'ember-ajax/request';
 
 export default Ember.Route.extend({
   model() {
-    return Ember.A([{
-      firstName: 'Lex',
-      lastName: 'Lacson'
-    },
-    {
-      firstName: 'a',
-      lastName: 'z'
-    }]);
+    return request('/assets/mock-data/issues.json')
+    .then(data => {
+      console.log('data:', data);
+
+      return data;
+    });
   },
 
   setupController(controller, model) {
     controller.setProperties({
       model: model,
       columns: Ember.computed(function() {
-        return [{
-          label: 'First Name',
-          valuePath: 'firstName',
-          width: '150px'
-        }, {
-          label: 'Last Name',
-          valuePath: 'lastName',
-          width: '150px'
-        }];
+        return [
+          {
+            label: 'Closed Timestamp',
+            valuePath: 'closedTimestamp',
+            width: '150px'
+          },
+          {
+            label: 'Customer Email',
+            valuePath: 'customerEmail',
+            width: '150px'
+          },
+          {
+            label: 'Customer Name',
+            valuePath: 'customerName',
+            width: '150px'
+          },
+          {
+            label: 'Description',
+            valuePath: 'description',
+            width: '150px'
+          },
+          {
+            label: 'Employee Name',
+            valuePath: 'employeeName',
+            width: '150px'
+          },
+          {
+            label: 'isOpen',
+            valuePath: 'isOpen',
+            width: '150px'
+          },
+          {
+            label: 'Submit Timestamp',
+            valuePath: 'submitTimestamp',
+            width: '150px'
+          }
+        ];
       }),
 
       table: Ember.computed('model', function() {
