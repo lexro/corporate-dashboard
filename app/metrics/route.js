fdfd947038c1a6669e832e206e755294c4049a59
Lex Lacson <lex.lacson.dev@gmail.com>
2017-01-02 09:49:24 -1000

    hook up metric mock data

--- app/metrics/route.js
+++ app/metrics/route.js
@@ -1,53 +1,54 @@
 import Ember from 'ember';
+import request from 'ember-ajax/request';
 
 export default Ember.Route.extend({
-  setupController(controller) {
+  model() {
+    return request('/assets/mock-data/metrics.json')
+    .then(data => {
+      console.log('data:', data);
+      const numIssues = Math.floor(Math.random() * 10);
+
+      return {
+        data: data,
+        numIssues: numIssues
+      };
+    });
+  },
+
+  _dataSort(a, b) {
+    console.log('a:', a);
+    return a.time.getTime() - b.time.getTime();
+  },
+
+  setupController(controller, model) {
+    let barData = [];
+    let lineData = [];
+    const data = model.data;
+
+    for(let i = 0; i < data.length; i++) {
+      const item = data[i];
+      const date = new Date(item.date);
+
+      barData.push({
+        time: date,
+        value: parseInt(item.issues, 10) || 1,
+        label: 'Number of Issues'
+      });
+
+      lineData.push({
+        time: date,
+        value: parseInt(item.customers, 10) || 1,
+        label: 'Number of Paying Customers'
+      });
+    }
+
+    lineData.sort(this._dataSort);
+    barData.sort(this._dataSort);
+
     controller.setProperties({
-      numIssues: 10,
-      barData: [
-        {
-          time: new Date(2013, 5, 1),
-          value: 64,
-          label: 'San Francisco'
-        },
-        {
-          time: new Date(2013, 5, 1),
-          value: 93,
-          label: 'Phoenix'
-        },
-        {
-          time: new Date(2013, 6, 1),
-          value: 66,
-          label: 'San Francisco'
-        },
-        {
-          time: new Date(2013, 6, 1),
-          value: 103,
-          label: 'Phoenix'
-        }
-      ],
-      lineData: [
-        {
-          time: new Date(2013, 5, 1),
-          value: 64,
-          label: 'San Francisco'
-        },
-        {
-          time: new Date(2013, 5, 1),
-          value: 93,
-          label: 'Phoenix'
-        },
-        {
-          time: new Date(2013, 6, 1),
-          value: 66,
-          label: 'San Francisco'
-        },
-        {
-          time: new Date(2013, 6, 1),
-          value: 103,
-          label: 'Phoenix'
-        }
-      ]
+      numIssues: model.numIssues,
+      barData: barData,
+      lineData: lineData
     });
   }
 });
