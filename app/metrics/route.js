af23bd745182ae1896e91ed209fafcc0f8078c90
Lex Lacson <lex.lacson.dev@gmail.com>
2017-01-01 22:26:22 -1000

    adding bar chart, line chart, issue number

--- app/metrics/route.js
+++ app/metrics/route.js
@@ -1,4 +1,53 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  setupController(controller) {
+    controller.setProperties({
+      numIssues: 10,
+      barData: [
+        {
+          time: new Date(2013, 5, 1),
+          value: 64,
+          label: 'San Francisco'
+        },
+        {
+          time: new Date(2013, 5, 1),
+          value: 93,
+          label: 'Phoenix'
+        },
+        {
+          time: new Date(2013, 6, 1),
+          value: 66,
+          label: 'San Francisco'
+        },
+        {
+          time: new Date(2013, 6, 1),
+          value: 103,
+          label: 'Phoenix'
+        }
+      ],
+      lineData: [
+        {
+          time: new Date(2013, 5, 1),
+          value: 64,
+          label: 'San Francisco'
+        },
+        {
+          time: new Date(2013, 5, 1),
+          value: 93,
+          label: 'Phoenix'
+        },
+        {
+          time: new Date(2013, 6, 1),
+          value: 66,
+          label: 'San Francisco'
+        },
+        {
+          time: new Date(2013, 6, 1),
+          value: 103,
+          label: 'Phoenix'
+        }
+      ]
+    });
+  }
 });
