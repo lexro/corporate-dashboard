5d7739125fd7bbbb448ff18cbf96a907a7862a06
Lex Lacson <lex.lacson.dev@gmail.com>
2017-01-04 13:32:42 -1000

    sorting

--- app/components/data-table/component.js
+++ app/components/data-table/component.js
@@ -2,27 +2,98 @@ import Ember from 'ember';
 import Table from 'ember-light-table';
 
 export default Ember.Component.extend({
-  model: null,
+ init() {
+    this._super(...arguments);
 
+    let table = new Table(this.get('columns'), this.get('model'), { enableSync: true });
+    this.set('table', table);
+  },
+  model: null,
   columns: Ember.computed(function() {
-    return [{
-      label: 'Avatar',
-      valuePath: 'avatar',
-      width: '60px',
-      sortable: false,
-      cellComponent: 'user-avatar'
-    }, {
-      label: 'First Name',
-      valuePath: 'firstName',
-      width: '150px'
-    }, {
-      label: 'Last Name',
-      valuePath: 'lastName',
-      width: '150px'
-    }];
+    return [
+      {
+        label: 'Closed Timestamp',
+        valuePath: 'closedTimestamp',
+        width: '75px'
+      },
+      {
+        label: 'Customer Email',
+        valuePath: 'customerEmail',
+        width: '170px'
+      },
+      {
+        label: 'Customer Name',
+        valuePath: 'customerName',
+        width: '150px'
+      },
+      {
+        label: 'Description',
+        valuePath: 'description',
+        width: '150px'
+      },
+      {
+        label: 'Employee Name',
+        valuePath: 'employeeName',
+        width: '150px'
+      },
+      {
+        label: 'isOpen',
+        valuePath: 'isOpen',
+        width: '50px'
+      },
+      {
+        label: 'Submit Timestamp',
+        valuePath: 'submitTimestamp',
+        width: '75px'
+      }
+    ];
   }),
 
-  table: Ember.computed('model', function() {
-   return new Table(this.get('columns'), this.get('model'));
-  })
+  displayedRows: Ember.observer('model', 'sort', 'filter', function() {
+    const model = this.get('model');
+    const sort = this.get('sort');
+
+    if (sort) {
+      model.sort((a, b) => {
+        const valueA = a[sort.valuePath];
+        const valueB = b[sort.valuePath];
+
+        if (sort.isAscending) {
+          if (valueA < valueB) {
+            return -1;
+          } else if (valueA > valueB) {
+            return 1;
+          }
+
+          return 0;
+        }
+
+        if (valueB < valueA) {
+          return -1;
+        } else if (valueB > valueA) {
+          return 1;
+        }
+
+        return 0;
+      });
+    }
+
+    const table = this.get('table');
+    table.setRows(model);
+  }),
+
+  sort: null,
+
+  filter: null,
+
+  actions: {
+    onColumnClick(column) {
+      if (column.sorted) {
+        this.set('sort', {
+          valuePath: column.get('valuePath'),
+          isAscending: column.ascending
+        });
+      }
+    }
+  }
 });
--- app/data/route.js
+++ app/data/route.js
@@ -1,63 +1,14 @@
 import Ember from 'ember';
-import Table from 'ember-light-table';
 import request from 'ember-ajax/request';
 
 export default Ember.Route.extend({
   model() {
-    return request('/assets/mock-data/issues.json')
-    .then(data => {
-      console.log('data:', data);
-
-      return data;
-    });
+    return request('/assets/mock-data/issues.json');
   },
 
   setupController(controller, model) {
     controller.setProperties({
-      model: model,
-      columns: Ember.computed(function() {
-        return [
-          {
-            label: 'Closed Timestamp',
-            valuePath: 'closedTimestamp',
-            width: '75px'
-          },
-          {
-            label: 'Customer Email',
-            valuePath: 'customerEmail',
-            width: '150px'
-          },
-          {
-            label: 'Customer Name',
-            valuePath: 'customerName',
-            width: '100px'
-          },
-          {
-            label: 'Description',
-            valuePath: 'description',
-            width: '150px'
-          },
-          {
-            label: 'Employee Name',
-            valuePath: 'employeeName',
-            width: '100px'
-          },
-          {
-            label: 'isOpen',
-            valuePath: 'isOpen',
-            width: '50px'
-          },
-          {
-            label: 'Submit Timestamp',
-            valuePath: 'submitTimestamp',
-            width: '75px'
-          }
-        ];
-      }),
-
-      table: Ember.computed('model', function() {
-       return new Table(this.get('columns'), this.get('model'));
-      })
+      model: model
     });
   }
 });
